30e63ba13febaa7b82b5e26096a39f6caafb9a30
Nattawut Singhchai <jadedragon17650@gmail.com>
2016-01-10 22:21:16 +0700

    Framework build

---
+++ EasyTransition/EasyTransition.h
@@ -0,0 +1,19 @@
+//
+//  EasyTransition.h
+//  EasyTransition
+//
+//  Created by Nattawut Singhchai on 1/10/16.
+//  Copyright © 2016 Nattawut Singhchai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyTransition.
+FOUNDATION_EXPORT double EasyTransitionVersionNumber;
+
+//! Project version string for EasyTransition.
+FOUNDATION_EXPORT const unsigned char EasyTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyTransition/PublicHeader.h>
+
+
